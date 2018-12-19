37d869edc34408b8a8c4704597f4238891d85f62
Nikola Kosaric <nikolakosaric@gmail.com>
2018-12-19 00:30:20 -0500

    Blinds can be controlled from phone utilizing ESP

--- Embedded Final/main.c
+++ Embedded Final/main.c
@@ -25,6 +25,8 @@ char positionArray[] = {'P','o','s','i','t','i','o','n'};
 int toggleArray[] = {'T','o','g','g','l','e'};
 int lightArray[] = {'L','i','g','h','t'};
 
+int messageReceived = 0;
+
 void configurePWM() {
     P1DIR |= BIT4;                              // Sets P1.2 to the output direction
     P1SEL |= BIT4;                              // Connects P1.2 to TA CCR0 Capture
@@ -32,17 +34,8 @@ void configurePWM() {
                                                 // of 1, sets the clock to up mode, and initially clears
                                                 // the clock
     TA0CCR0 = 656;                              // Sets the period of the PWM cycle
-    TA0CCR3 =72;                                // Sets the duty cycle
+    TA0CCR3 =48;                                // Sets the duty cycle
     TA0CCTL3 = OUTMOD_7;                        // Sets TA0 to set/reset
-}
-
-void configureADC12() {
-    ADC12CTL0 = ADC12SHT02 + ADC12ON;           // Turns on ADC12
-    ADC12CTL1 = ADC12SHP;                       // Sources the sampcon signal from the sampling timer
-    ADC12IE = BIT0;                             // Enables ADC12 interrupts
-    ADC12CTL0 |= ADC12ENC;                      // Enales ADC12 conversion
-    P6SEL |= BIT0;                              // P6.0 is configures as the ADC12 input
-    P1DIR |= BIT0;                              // P1.0 is set to the output direction
 }
 
 void configureUART0() {
@@ -69,34 +62,75 @@ void configureUART1() {
     UCA1IE |= UCTXIE;                           // Enables TX based interrupts
 }
 
-int main(void)
-{
+int main(void) {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-    //configurePWM();
-    //configureADC12();
+    configurePWM();
     configureUART0();
-    //configureUART1();
 
-     while (1) {
-         ADC12CTL0 |= ADC12SC;                       // Start sampling and conversion of ADC12
-         __bis_SR_register(LPM0_bits + GIE);         // LPM0, ADC12_ISR will force exit
-     }
+    __bis_SR_register(GIE);                     // LPM0, ADC12_ISR will force exit
+    while (1) {
+        while (messageReceived == 1) {
+
+            int int1 = 0;
+            int int2 = 0;
+            int int3 = 0;
+
+            if (valueArray[2] == 0) {
+                if (valueArray[1] == 0) {
+                    valueInt = valueArray[0] - '0';
+                } else {
+                    int1 = (valueArray[0] - '0') * 10;
+                    int2 = (valueArray[1] - '0');
+                    valueInt = int1 + int2;
+                }
+            } else {
+                int1 = (valueArray[0] - '0') * 100;
+                int2 = (valueArray[1] - '0') * 10;
+                int3 = (valueArray[2] - '0');
+                valueInt = int1 + int2 + int3;
+            }
+
+            int i = 0;
+
+            for (i = 0; i < 8; i++) {
+                if (topicArray[i] == positionArray[i] && toggle == 0 && light == 0) {
+                    position = 1;
+                } else if (topicArray[i] == toggleArray[i] && position == 0 && light == 0) {
+                    toggle = 1;
+                } else if (topicArray[i] == lightArray[i] && position == 0 && toggle == 0) {
+                    light = 1;
+                }
+            }
+
+            for (i = 0; i < 8; i++) {
+                topicArray[i] = 0;
+            }
+            for (i = 0; i < 3; i++) {
+                valueArray[i] = 0;
+            }
+
+            if (position == 1) {
+                if (valueInt > 100) {
+                                valueInt = 100;
+                } else if (valueInt < 0) {
+                                valueInt = 0;
+            }
+                            TA0CCR3 = valueInt * -0.18 + 48;
+            } else if (toggle == 1) {
+                if (valueInt > 1) {
+                    valueInt = 1;
+                } else if (valueInt < 0) {
+                    valueInt = 0;
+                }
+                valueInt = valueInt * 100;
+                TA0CCR3 = valueInt * -0.18 + 48;
+            }
+
+            messageReceived = 0;
+
+        }
+    }
 }
-/*
-#pragma vector = ADC12_VECTOR
-__interrupt void ADC12_ISR(void){
-  switch(ADC12IV) {
-  case  6:                                       // Vector 6:  ADC12IFG0
-      val = ADC12MEM0;
-      pos = (val / 4096.0) * 100;
-      TA0CCR3 = pos * 0.28 +23;
-
-      __bic_SR_register_on_exit(LPM0_bits);      // Exit active CPU
-
-  default: break;
-  }
-}
-*/
 
 #pragma vector=USCI_A0_VECTOR
 __interrupt void UART0(void) {
@@ -109,8 +143,7 @@ __interrupt void UART0(void) {
         if (input == 10 || input == 13){
             value = 1;
             counter = 0;
-
-            sscanf(valueArray, "%d", &valueInt);
+            messageReceived = 1;
         } else if (input == ':' || input == ' ') {
             topic = 1;
             counter = 0;
@@ -118,16 +151,12 @@ __interrupt void UART0(void) {
             topic = 0;
             value = 0;
             counter = 0;
-            /*
-            int i = 0;
+            messageReceived = 0;
 
-            for (i = 0; i < 8; i++) {
-                topicArray[i] = 0;
-            }
-            for (i = 0; i < 3; i++) {
-                valueArray[i] = 0;
-            }
-            */
+            position = 0;
+            toggle = 0;
+            light = 0;
+
         } else if (topic == 1) {
             valueArray[counter] = input;
             counter ++;
@@ -137,40 +166,3 @@ __interrupt void UART0(void) {
         }
     }
 }
-
-/*
-// UART1 interrupt vector
-#pragma vector=USCI_A1_VECTOR
-__interrupt void UART1(void) {
-    if (UCA1IFG & UCTXIFG) {                    // If the TX interrupt flag is triggered
-        UCA1IFG &= ~UCTXIFG;                    // Clears the TX interrupt flag
-        ADC12CTL0 |= ADC12SC;                   // Start sampling and conversion of ADC12
-    } if (UCA1IFG & UCRXIFG){                   // If the RX interrupt flag is triggered
-        input = UCA1RXBUF;
-
-        if (input == 10 || input == 13){
-            value = 1;
-            counter = 0;
-        } else if (input == ':' || input == ' ') {
-            topic = 1;
-            value = 0;
-            counter = 0;
-        } else if (input == '!') {
-            topic = 0;
-            value = 0;
-            counter = 0;
-        } else if (topic == 1 && value == 0) {
-            valueArray[counter] = input;
-            counter ++;
-        } else if (topic == 0 && value == 0) {
-            topicArray[counter] = input;
-            counter ++;
-        } else {
-            topic = 0;
-            value = 0;
-            counter = 0;
-        }
-
-    }
-}
-*/
